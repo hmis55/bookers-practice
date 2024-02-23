66a1568f4463117d2cb23d0ece20f24b50d4bc02
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-23 08:48:16 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+ 
+
+ def edit
+ end
+ 
+ def show
+   @book = book.new
+   @books = @user.books
+ end
+ 
+ def index
+ end
+ 
+ def update
+ end
+ 
+
+end
