1740b39337442ffdc4ced04b7bab746774d718f3
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-24 08:04:45 +0000

    user indexページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,33 @@
 class UsersController < ApplicationController
- 
+
 
  def edit
  end
- 
+
  def show
-   @book = book.new
-   @books = @user.books
+  @user = User.find(params[:id])
  end
- 
+
+ def create
+   @books = book.new(book_params)
+   @book.user_id = current_user_id
+   @book.save
+   redirect_to users_@path
+ end
+
+
  def index
+  @user = current_user
+  @book = Book.new
+  @users = User.all
  end
- 
+
  def update
  end
- 
 
+
+private
+  def book_params
+    params.require(:book).permit(:title,:opinion)
+  end
 end
