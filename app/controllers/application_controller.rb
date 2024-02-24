ac4f908e139530e2a6a496055f7e2959ef867097
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-24 05:10:56 +0000

    ログイン画面まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+# sign inしたときの遷移先
+  def after_new_user_session_for(resource)
+    books_path
+  end
+# log in したときの遷移先
+  def after_new_user_registration_for(resource)
+    books_path
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
